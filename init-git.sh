47cf9a83791e3b20cf237f2c3448b071f9c76aae
xton13 <xton13@users.noreply.github.com>
2018-02-20 18:31:09 -0600

    Removed hardcoded username,

--- init-git.sh
+++ init-git.sh
@@ -5,5 +5,5 @@ curl -u $USERNAME https://api.github.com/user/repos -d '{ "name": "'$REPONAME'",
 git init
 git add -A
 git commit -m "Init"
-git remote add origin git@github.com:xton13/$REPONAME
+git remote add origin git@github.com:$USERNAME/$REPONAME
 git push origin master
